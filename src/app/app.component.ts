68a0016da9cd8c2d3f6d303f7db31ec3a850c177
alvaro <alvaroriveravivero@gmail.com>
2025-02-27 13:26:43 +0100

    subete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de Empleados';
+  mensaje="";
+  registrado=false;
+  nombre:string="";
+  apellido:string="";
+  entradas: { titulo: string }[];
+
+  constructor() {
+    this.entradas = [
+      { titulo: "titulo1" },
+      { titulo: "titulo2" },
+      { titulo: "titulo3" },
+      { titulo: "titulo4" },
+      { titulo: "titulo5" }
+    ];
+  }
+
+  registrarUsuario(){
+    this.registrado=true
+
+    this.mensaje="usuario registrado con éxito"
+  }
 }
